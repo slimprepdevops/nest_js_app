6b57b0abe8458f971ecd7ec52c8e599adf03a748
Benjamin Isong <47520669+beisong7@users.noreply.github.com>
2022-10-08 19:40:39 +0100

    Update class_activity.sh

--- class_activity.sh
+++ class_activity.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
-curl ifconfig.co
-echo "class activity called"
-echo "class activity shell mode"
+echo "running apt update"
+sudo apt update
 
+echo "installing nodejs"
+sudo apt install nodejs -y
+
+echo "installing NPM"
+sudo apt install npm -y
+
+echo "installing yarn"
+sudo npm install -g yarn -y
+
+echo "installing pm2"
+sudo npm install pm2 -g
+
+echo "installing project"
+npm install -y
+
+echo "Building project"
+yarn build
+
+echo "changing directory"
+cd dist
+
+echo "starting project in directory"
+pm2 start main.js
+
+echo "list running apps"
+pm2 list
+
+echo "Application deployed successfully"
